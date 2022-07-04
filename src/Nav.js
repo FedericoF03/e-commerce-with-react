7c8974f25495b89d7b7d5db2366b519d45f4558c
federico.kpo@hotmail.com.ar <federicofernandeztec@gmail.com>
2022-07-03 22:08:40 -0300

    Se instalo react y react router, se crearon 2 componentes y una estructura basica

---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import {BrowserRouter, NavLink} from 'react-router-dom'
+
+export default function NavLinks(){
+    return(
+        <div>
+            <ul>
+                <li>
+                    <NavLink></NavLink>
+                </li>
+                <li>
+                    <NavLink></NavLink>
+                </li>
+                <li>
+                    <NavLink></NavLink>
+                </li>
+                <li>
+                    <NavLink></NavLink>
+                </li>
+                <li>
+                    <NavLink></NavLink>
+                </li>
+                <li>
+                    <NavLink></NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
